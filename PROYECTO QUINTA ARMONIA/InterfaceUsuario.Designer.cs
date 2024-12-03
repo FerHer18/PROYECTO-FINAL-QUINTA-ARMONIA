d9413ebb577a92bd8e9eb4b1fd1b87a301b65815
Valeria <valeria05p@outlook.com>
2024-12-03 17:53:58 -0600

    Fondo y parche del fondo

--- PROYECTO QUINTA ARMONIA/InterfaceUsuario.Designer.cs
+++ PROYECTO QUINTA ARMONIA/InterfaceUsuario.Designer.cs
@@ -28,21 +28,40 @@
         /// </summary>
         private void InitializeComponent()
         {
+            patch = new PictureBox();
+            ((System.ComponentModel.ISupportInitialize)patch).BeginInit();
             SuspendLayout();
+            // 
+            // patch
+            // 
+            patch.BackColor = Color.Transparent;
+            patch.BackgroundImage = Properties.Resources.backgroundPatch224x164;
+            patch.BackgroundImageLayout = ImageLayout.None;
+            patch.Location = new Point(352, 295);
+            patch.Name = "patch";
+            patch.Size = new Size(185, 162);
+            patch.TabIndex = 0;
+            patch.TabStop = false;
             // 
             // InterfaceUsuario
             // 
-            AutoScaleDimensions = new SizeF(10F, 25F);
+            AutoScaleDimensions = new SizeF(7F, 15F);
             AutoScaleMode = AutoScaleMode.Font;
-            ClientSize = new Size(1650, 958);
-            Margin = new Padding(4, 4, 4, 4);
+            BackgroundImage = Properties.Resources.Fondo;
+            BackgroundImageLayout = ImageLayout.Center;
+            ClientSize = new Size(944, 541);
+            Controls.Add(patch);
+            Margin = new Padding(3, 2, 3, 2);
             Name = "InterfaceUsuario";
             StartPosition = FormStartPosition.CenterScreen;
             Text = "InterfaceUsuario";
             Load += InterfaceUsuario_Load;
+            ((System.ComponentModel.ISupportInitialize)patch).EndInit();
             ResumeLayout(false);
         }
 
         #endregion
+
+        private PictureBox patch;
     }
 }
